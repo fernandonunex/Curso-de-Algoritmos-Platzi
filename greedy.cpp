1548f355aa7b22c36f0b0f9554948bdeb3a1aefb
FernandoOmen <fer_bsof@outlook.com>
2020-08-05 22:41:09 -0500

    Greedy algorithm just say how many coins will give

--- greedy.cpp
+++ greedy.cpp
@@ -3,16 +3,17 @@
 
 using namespace std;
 
-int greedyChange(int coinSet, int n ,int N){
+int greedyChange(int coinSet[], int n ,int N){
     if(N==0)
         return 0;
     if(N < 0)
         return INT_MAX;
     
     int coins = INT_MAX;
+    int i;
 
     for ( i = 0; i < n; i++)
-    reccorremos todo nuestro set de monedas aka para dar cambio
+    // reccorremos todo nuestro set de monedas aka para dar cambio
     {
         int res = greedyChange(coinSet, n, N-coinSet[i]);
         if(res != INT_MAX)
@@ -20,8 +21,17 @@ int greedyChange(int coinSet, int n ,int N){
     }
 
     return coins;
+
+}
+
+int main(int argc, char const *argv[])
+{
+    int coinSet[] = {1,5,10,15,20};
+    int n = sizeof(coinSet)/sizeof(coinSet[0]);
+    int N = 20;
+
+    cout<<"The minimum change using coins is:"
+        << greedyChange(coinSet, n, N);
     
-
-
-
+    return 0;
 }
